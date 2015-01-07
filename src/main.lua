ba4206fc43c2d805b5f6dbf5bbda928b1d5d57a6
mrcpj1998 <mrcpj1998@gmail.com>
2015-01-07 17:47:40 +0100

    Added very basic bullet
    I dont even know if this works. Glorypants, u maed tis.

--- src/main.lua
+++ src/main.lua
@@ -2,6 +2,7 @@
 require "libraries.loop"
 require "libraries.click"
 require "libraries.keyboard"
+require "libraries.Bullet"
 asdf = 0
 --gets called when the game starts
 function love.load()
@@ -15,15 +16,23 @@ end
 --gets called when mouse is clicked
 function love.mousepressed(x, y, button)
    processClick()
+   initialize(x, y)
 end
 
 --gets called when a key is pressed
 function love.keypressed(key)
-  proccesKey(key)
+  proccessKey(key)
 end
 
 --is continuely updated
 --all drawing on screen must happen here
 function love.draw()
     love.graphics.print('Hello World!', asdf, 300)
+    function drawEntity(x, y, istext, text, art)
+      if istext then
+        love.graphics.print(text,x,y)
+      else
+    
+      end
+    end
 end
