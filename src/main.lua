f001548914fe5d7ee8f123866d6861b56585d31a
Captain Glorypants <captglorypants@hotmail.com>
2015-01-07 15:34:47 +0100

    testing
    I do stuff

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,13 @@
+asdf = 0
 function love.load()
-
+  while asdf < 10 do
+    asdf = asdf + 1
+  end
 end
 
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+    love.graphics.print('Hello World!', 200+asdf*20, 300)
+    love.graphics.print(asdf, 30, 30)
 end
 
 
