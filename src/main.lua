a9d463449613f9e593082984c8b5b618adda88f9
Captain Glorypants <captglorypants@hotmail.com>
2015-01-07 15:56:40 +0100

    It's alive

--- src/main.lua
+++ src/main.lua
@@ -1,13 +1,10 @@
 asdf = 0
-function love.load()
-  while asdf < 10 do
-    asdf = asdf + 1
-  end
+function love.update(dt)
+  asdf  = asdf + 50 * dt
 end
 
 function love.draw()
-    love.graphics.print('Hello World!', 200+asdf*20, 300)
-    love.graphics.print(asdf, 30, 30)
+    love.graphics.print('Hello World!', asdf, 300)
 end
 
 
