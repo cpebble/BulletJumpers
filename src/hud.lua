5775e334cd0fd91ccffc6b21453db30d9cef8e08
Captain Glorypants <captglorypants@hotmail.com>
2015-01-27 14:22:24 +0100

    insignificant

--- src/hud.lua
+++ src/hud.lua
@@ -5,6 +5,6 @@ function drawHUD()
   love.graphics.rectangle("fill",10,10,50,100)
   love.graphics.setColor({255,150,0})
   love.graphics.rectangle("fill",10,110-player.momentum/maxMomentum*100,50,player.momentum/maxMomentum*100)
-  love.graphics.setColor({0,200,0,100})
+  love.graphics.setColor({0,200,0})
   love.graphics.rectangle("fill",10,110-math.abs(player.xv)/maxMomentum*100,50,math.abs(player.xv)/maxMomentum*100)
 end
